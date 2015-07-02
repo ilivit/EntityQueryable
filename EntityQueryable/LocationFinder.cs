aa463750cddad75c11c289d5db40a6c52eb8d0a0
Ilya Petukhov <ioprst@gmail.com>
2015-07-02 19:04:19 +0300

    initial commit

---
+++ EntityQueryable/LocationFinder.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace EntityQueryable
+{
+    internal class LocationFinder : ExpressionVisitor
+    {
+        private Expression expression;
+        private List<string> locations;
+
+        public LocationFinder(Expression exp)
+        {
+            this.expression = exp;
+        }
+
+        public List<string> Locations
+        {
+            get
+            {
+                if (locations == null)
+                {
+                    locations = new List<string>();
+                    this.Visit(this.expression);
+                }
+                return this.locations;
+            }
+        }
+
+        protected override Expression VisitBinary(BinaryExpression be)
+        {
+            if (be.NodeType == ExpressionType.Equal)
+            {
+                if (ExpressionTreeHelpers.IsMemberEqualsValueExpression(be, typeof(Place), "Name"))
+                {
+                    locations.Add(ExpressionTreeHelpers.GetValueFromEqualsExpression(be, typeof(Place), "Name"));
+                    return be;
+                }
+                else if (ExpressionTreeHelpers.IsMemberEqualsValueExpression(be, typeof(Place), "State"))
+                {
+                    locations.Add(ExpressionTreeHelpers.GetValueFromEqualsExpression(be, typeof(Place), "State"));
+                    return be;
+                }
+                else
+                    return base.VisitBinary(be);
+            }
+            else
+                return base.VisitBinary(be);
+        }
+    }
+}
---
+++ EntityQueryable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityQueryable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var terraPlaces = new QueryableTerraServerData<Place>();
+
+            var query = from place in terraPlaces
+                        where place.Name == "place1"
+                        select place.PlaceType;
+
+            foreach (PlaceType placeType in query)
+                Console.WriteLine(placeType);
+
+        }
+    }
+
+
+    
+}
